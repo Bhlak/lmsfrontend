563144f957e130f965307c8302f32fb20bde5714
Adekoya Ibukunoluwa <100376947+Bhlak@users.noreply.github.com>
2025-08-20 13:14:59 +0100

    request method fix

--- src/pages/Loan.jsx
+++ src/pages/Loan.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Loan = () => {
+  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem("User")));
+  const fetchLoans = async () => {
+    // console.log("Fetching");
+    try {
+      let token = sessionStorage.getItem("Token");
+      const details = {
+        email: user.email,
+      };
+      const res = await fetch(`https://lms-7czt.onrender.com/books/loans/`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `token ${token}`,
+        },
+        method: "POST",
+        body: JSON.stringify(details),
+      });
+
+      const data = await res.json();
+      // console.log(data);
+      console.log(data);
+      let resources = data;
+      // console.log("Storage");
+      sessionStorage.setItem("Books", JSON.stringify(resources));
+      window.dispatchEvent(new Event("bookStorage"));
+      // setFetchedBooks(resources);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  fetchLoans();
   return (
     <HomeContainer>
       <Content>Loaning</Content>
